ff57779a2beaed3b5ea57649d1eef342a895c594
codedbychavez <codedbychavez@gmail.com>
2022-03-15 06:45:46 -0400

    added grading logic

--- src/components/templates/QuizTemplate.js
+++ src/components/templates/QuizTemplate.js
@@ -1,126 +1,97 @@
-import { useState } from "react";
 import styled from "styled-components";
 
 import Card from "react-bootstrap/Card";
 import { StyledSmallSpacer } from "../molecules/Spacer";
 import Instructions from "../organisms/Instructions";
-import Form from "react-bootstrap/Form"
+import Form from "react-bootstrap/Form";
 import { MathQuestions } from "../../data/math_questions";
 import { Button } from "react-bootstrap";
+import { GradeQuiz } from "../../functions/grade_quiz";
 
 const QuizTemplate = (props) => {
+  const answers = [];
 
-    const [answers, setAnswers] = useState([])
+  const instructions = [
+    "Read all instructions carefully.",
+    "Submit your quiz when completed by clicking the submit button at the end of the quiz.",
+  ];
 
-    const instructions = [
-        "Read all instructions carefully.",
-        "Submit your quiz when completed by clicking the submit button at the end of the quiz."
-    ]
+  const mathQuestions = MathQuestions;
 
-    const mathQuestions = MathQuestions;
-
-    const handleAnswer = (questionId, option) => {
-        let answer = {
-            questionId: questionId,
-            option: option,
-        }
-
-        console.log(answers)
-
+  const handleAnswer = (questionId, option) => {
+    let answer = {
+      questionId: questionId,
+      option: option,
+    };
+    if (!answers.some((answer) => answer.questionId === questionId)) {
+      answers.push(answer);
+    } else {
+      let answerIndex = answers.indexOf(answer);
+      answers.splice(answerIndex, 1, answer);
     }
+  };
 
-    return (
-        <div>
-            <StyledSmallSpacer>
-                <Instructions instructions={instructions} quiz={props.name}/>
-            </StyledSmallSpacer>
+  const handleSubmit = (event) => {
+    const score = GradeQuiz(mathQuestions.length, answers);
+    console.log("You scored: ", score);
+    event.preventDefault();
+  };
 
-            <StyledSmallSpacer>
-                <Card>
-                    <Card.Header>
-                        <Card.Title>
-                            {props.name}
-                        </Card.Title>
-                    </Card.Header>
+  return (
+    <div>
+      <StyledSmallSpacer>
+        <Instructions instructions={instructions} quiz={props.name} />
+      </StyledSmallSpacer>
 
-                    <StylesCardBody>
-                       
-                        {
-                        mathQuestions.map((question) => {
+      <StyledSmallSpacer>
+        <Card>
+          <Card.Header>
+            <Card.Title>{props.name}</Card.Title>
+          </Card.Header>
+
+          <StylesCardBody>
+            <Form onSubmit={handleSubmit}>
+              {mathQuestions.map((question) => {
+                return (
+                  <StyledSmallSpacer key={question.id}>
+                    <Card>
+                      <Card.Body>
+                        <p>{question.question}</p>
+
+                        <div>
+                          {question.options.map((option, optionIndex) => {
                             return (
-                                <StyledSmallSpacer key={question.id}>
-                                <Card>
-                                    <Card.Body>
-                                        <p>{question.question}</p>
+                              <Form.Check
+                                key={optionIndex}
+                                inline
+                                label={option.label}
+                                name={`${question.id}-group`}
+                                type="radio"
+                                id={`${question}-${option}-${optionIndex}`}
+                                onClick={() =>
+                                  handleAnswer(question.id, option)
+                                }
+                              />
+                            );
+                          })}
+                        </div>
+                      </Card.Body>
+                    </Card>
+                  </StyledSmallSpacer>
+                );
+              })}
 
-                                        <div>
-
-                                            {
-                                                question.options.map((option, optionIndex) => {
-                                                    return (
-                                                        <Form.Check
-                                                                key={optionIndex}
-                                                                inline
-                                                                label={option.label}
-                                                                name={`${question.id}-group`}
-                                                                type="radio"
-                                                                id={`${question}-${option}-${optionIndex}`}
-                                                                onClick={() => handleAnswer(question.id, option)}
-                                                            />
-                                                    )
-                                                })
-                                            }
-
-                                        </div>
-                            
-        
-                                    </Card.Body>
-                                </Card>
-                                </StyledSmallSpacer>
-                            )
-                        })
-                        
-                        
-                        }
-
-                        {/* <Form>
-                            What is the sum of 4 + 5 ?
-                            {['checkbox'].map((type) => (
-                                <div key={`inline-${type}`} className="mb-3">
-                                <Form.Check
-                                    inline
-                                    label="1"
-                                    name="group1"
-                                    type={type}
-                                    id={`inline-${type}-1`}
-                                />
-                                <Form.Check
-                                    inline
-                                    label="2"
-                                    name="group1"
-                                    type={type}
-                                    id={`inline-${type}-2`}
-                                />
-                                <Form.Check
-                                    inline
-                                    label="3"
-                                    type={type}
-                                    id={`inline-${type}-3`}
-                                />
-                                </div>
-                            ))}
-                        </Form> */}
-
-                        <Button>Submit</Button>
-                    </StylesCardBody>
-                </Card>
-            </StyledSmallSpacer>
-        </div>
-    )
-}
+              <Button type="submit">Submit</Button>
+            </Form>
+          </StylesCardBody>
+        </Card>
+      </StyledSmallSpacer>
+    </div>
+  );
+};
 
 const StylesCardBody = styled(Card.Body)`
-    text-align: left;
-`
+  text-align: left;
+`;
 
 export default QuizTemplate;
--- src/functions/grade_quiz.js
+++ src/functions/grade_quiz.js
@@ -1,4 +1,13 @@
-export const GradeQuiz = (questions, answers) => {
+export const GradeQuiz = (totalQuestions, answers) => {
+    let correctAnswers = 0;
+
+    for (let i = 0; i < answers.length; i++) {
+        if (answers[i].option.isCorrect) {
+            correctAnswers++;
+        }
+    }
+
+    return `${correctAnswers}/${totalQuestions}`
     
 
 }
